dd7667c9088646a389f1ffe2c617f6d8d6b0b83c
nurlan <baitassovnurlan1507@gmail.com>
2024-10-21 15:56:51 +0500

    feat: tests for 01

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,82 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        action: Action.Add,
+        a: 1,
+        b: 2,
+      }),
+    ).toBe(3);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        action: Action.Subtract,
+        a: 5,
+        b: 3,
+      }),
+    ).toBe(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        action: Action.Multiply,
+        a: 4,
+        b: 3,
+      }),
+    ).toBe(12);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        action: Action.Divide,
+        a: 8,
+        b: 2,
+      }),
+    ).toBe(4);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        action: Action.Exponentiate,
+        a: 2,
+        b: 3,
+      }),
+    ).toBe(8);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        action: 'invalid',
+        a: 2,
+        b: 3,
+      }),
+    ).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        action: Action.Add,
+        a: 'invalid',
+        b: 3,
+      }),
+    ).toBeNull();
+
+    expect(
+      simpleCalculator({
+        action: Action.Add,
+        a: 2,
+        b: 'invalid',
+      }),
+    ).toBeNull();
   });
 });
